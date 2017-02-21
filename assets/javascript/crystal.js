032e32286c8e70fd647f31ca5076a01b54d51975
Sandra Seay <Sandra Seay>
2017-02-20 22:54:59 -0500

    updated javascript works now

--- assets/javascript/crystal.js
+++ assets/javascript/crystal.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 //Global Varible 
 var loss = 0;
 var win = 0;
-var crystalValue = Math.floor(Math.random() * (102)+ 19)
+var crystalValue = Math.floor(Math.random() * (102)+ 19);
 var gemArray = [];
-var total = 0
+var total = 0;
 
-//function to genrate random gem values
+//function to generate random gem values
 	function gemFunc() {
 		for (var i = 0; i < 4; i++){
 		var gem = Math.floor(Math.random() * 12) + 1 ;
@@ -18,14 +18,16 @@ var total = 0
    gemFunc();
    $('.numberToMatch').html(crystalValue);
    //crystal value displays in console
-   console.log(gemArray);
+   	console.log(crystalValue)
+   		console.log(gemArray);
 
 
   $('.pic1').on("click", function(){
   //take the value and push it to total score
   // gemFunc();
-	var gem1value = (gemArray[0]);
-	total = total + gem1value;
+	var gem1value = gemArray[0];
+	console.log(gem1value);
+	total = total + gemArray[0];
 	$('.score').html(total);
 	checkit();
 });
@@ -33,51 +35,55 @@ var total = 0
   $('.pic2').on("click", function(){
   //take the value and push it to total score
 	//gemFunc();
-	var gem2value = (gemArray[1]);
-	total = total + gem2value;
+	var gem2value = gemArray[1];
+	console.log(gem2value);
+	total = total + gemArray[1];
 	$('.score').html(total);
 	checkit();
 });
 
   $('.pic3').on("click", function(){
 //take the value and push it to total score
-	
-	var gem3value = (gemArray[2]);
-	total = total + gem3value;
+	var gem3value = gemArray[2];
+	console.log(gem3value);
+	total = total + gemArray[2];
 	$('.score').html(total);
 	checkit();
  });
 
 $('.pic4').on("click", function(){
 //take the value and push it to total score
-	var gem4value = (gemArray[3]);
-	total = total + gem4value;
+	var gem4value = gemArray[3];
+	console.log(gem4value);
+	total = total + gemArray[3];
 	$('.score').html(total);
 	checkit();
  });
-
+//check to see if crystal value equals total
 function checkit(){
 if (crystalValue === total) {
 	$("#winlossmessage").html("You won that round!");
     win = win + 1;
     $("#wins").html(win);
-   //reset
+   //reset the game
    reset();
 	}
 else if (crystalValue < total) {
 	$("#winlossmessage").html("You lost that round!");
-    $("#losses").html(loss++);
-   //reset
+	loss = loss + 1;
+    $("#losses").html(loss);
+   //reset the game
    reset();
 	}
 };
 
+//reset the game
 function reset(){
 	gemArray.length = 0;
 	gemFunc();
 	crystalValue = Math.floor(Math.random() * (102)+ 19)
 	$('.numberToMatch').html(crystalValue);
-	adder = 0;
+	total = 0;
 	$('.score').html(total);
 	console.log(gemArray)
 }
